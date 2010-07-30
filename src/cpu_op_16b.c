bf42bd03f550bc47f72a4af3caf0fceafe954e89
Gabriel Corvalan <just.the.boss@gmail.com>
2010-07-31 01:52:36 +0200

    Fix types

--- src/cpu_op_16b.c
+++ src/cpu_op_16b.c
@@ -29,7 +29,7 @@ cpu_op_8b_sub (REG8 *dest, REG8 value, UINT32 *zero, UINT32 *negative, UINT32 *h
 }
 
 void
-cpu_op_8b_inc (REG8 *dest, unsigned int *negative, unsigned int *half_carry, unsigned int *zero) {
+cpu_op_8b_inc (REG8 *dest, UINT32 *negative, UINT32 *half_carry, UINT32 *zero) {
   *negative = FALSE;
   *half_carry = ((*dest & 0x0F) == 0x0F) ? TRUE : FALSE;
   *zero = (*dest == 0xFF) ? TRUE : FALSE;
@@ -37,7 +37,7 @@ cpu_op_8b_inc (REG8 *dest, unsigned int *negative, unsigned int *half_carry, uns
 }
 
 void
-cpu_op_8b_dec (REG8 *dest, unsigned int *negative, unsigned int *half_carry, unsigned int *zero) {
+cpu_op_8b_dec (REG8 *dest, UINT32 *negative, UINT32 *half_carry, UINT32 *zero) {
   *negative = TRUE;
   *half_carry = ((*dest & 0x0F) == 0x00) ? TRUE : FALSE;
   *zero = (*dest == 0x01) ? TRUE : FALSE;
@@ -45,7 +45,7 @@ cpu_op_8b_dec (REG8 *dest, unsigned int *negative, unsigned int *half_carry, uns
 }
 
 void
-cpu_op_8b_and (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_and (REG8 *dest, BYTE value, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *dest &= value;
   *zero = (*dest == 0) ? TRUE : FALSE;
   *negative = FALSE;
@@ -54,7 +54,7 @@ cpu_op_8b_and (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negativ
 }
 
 void
-cpu_op_8b_or (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_or (REG8 *dest, BYTE value, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *dest |= value;
   *zero = (*dest == 0) ? TRUE : FALSE;
   *negative = FALSE;
@@ -63,7 +63,7 @@ cpu_op_8b_or (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negative
 }
 
 void
-cpu_op_8b_xor (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_xor (REG8 *dest, BYTE value, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *dest ^= value;
   *zero = (*dest == 0) ? TRUE : FALSE;
   *negative = FALSE;
@@ -72,7 +72,7 @@ cpu_op_8b_xor (REG8 *dest, BYTE value, unsigned int *zero, unsigned int *negativ
 }
 
 void
-cpu_op_8b_bit (REG8 *dest, unsigned short int bit, unsigned int *zero, unsigned int *negative, unsigned int *half_carry) {
+cpu_op_8b_bit (REG8 *dest, unsigned short int bit, UINT32 *zero, UINT32 *negative, UINT32 *half_carry) {
   UINT8 mask = 0x01 << bit;
   *zero = (*dest & mask) == 0 ? TRUE : FALSE;
   *negative = FALSE;
@@ -80,7 +80,7 @@ cpu_op_8b_bit (REG8 *dest, unsigned short int bit, unsigned int *zero, unsigned 
 }
 
 void
-cpu_op_8b_rol (REG8 *target, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_rol (REG8 *target, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   BOOL c = *carry;
   *carry = *target >> 7;
   *target = (*target << 1) | c;
@@ -90,7 +90,7 @@ cpu_op_8b_rol (REG8 *target, unsigned int *zero, unsigned int *negative, unsigne
 }
 
 void
-cpu_op_8b_rlc (REG8 *target, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_rlc (REG8 *target, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *carry = *target >> 7;
   *target = (*target << 1) | *carry;
   *zero = (*target == 0) ? TRUE : FALSE;
@@ -99,7 +99,7 @@ cpu_op_8b_rlc (REG8 *target, unsigned int *zero, unsigned int *negative, unsigne
 }
 
 void
-cpu_op_8b_ror (REG8 *target, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_ror (REG8 *target, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   BOOL c = *carry;
   *carry = *target & 0x01;
   *target = (*target >> 1) | (c << 7);
@@ -109,7 +109,7 @@ cpu_op_8b_ror (REG8 *target, unsigned int *zero, unsigned int *negative, unsigne
 }
 
 void
-cpu_op_8b_rrc (REG8 *target, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_rrc (REG8 *target, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *carry = *target & 0x01;
   *target = (*target >> 1) | (*carry << 7);
   *zero = (*target == 0) ? TRUE : FALSE;
@@ -118,7 +118,7 @@ cpu_op_8b_rrc (REG8 *target, unsigned int *zero, unsigned int *negative, unsigne
 } 
 
 void
-cpu_op_8b_cmp (REG8 reg1, REG8 reg2, unsigned int *zero, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_8b_cmp (REG8 reg1, REG8 reg2, UINT32 *zero, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *zero = (reg1 == reg2) ? TRUE : FALSE;
   *negative = TRUE;
   *half_carry = ((reg1 & 0x0F) < (reg2 & 0x0F)) ? TRUE : FALSE;
@@ -126,7 +126,7 @@ cpu_op_8b_cmp (REG8 reg1, REG8 reg2, unsigned int *zero, unsigned int *negative,
 }
 
 void
-cpu_op_8b_cpl (REG8 *target, unsigned int *negative, unsigned int *half_carry) {
+cpu_op_8b_cpl (REG8 *target, UINT32 *negative, UINT32 *half_carry) {
   *target = ~(*target);
   *negative = TRUE;
   *half_carry = TRUE; 
@@ -158,7 +158,7 @@ cpu_op_16b_dec (REG16 *dest) {
 }
 
 void
-cpu_op_16b_add (REG16 *dest, WORD value, unsigned int *negative, unsigned int *half_carry, unsigned int *carry) {
+cpu_op_16b_add (REG16 *dest, WORD value, UINT32 *negative, UINT32 *half_carry, UINT32 *carry) {
   *negative = FALSE;
   *half_carry = (dest->r_8.l + (value & 0xFF) > 0xFF) ? TRUE : FALSE;
   *carry = (dest->r_16 + value > 0xFFFF) ? TRUE : FALSE;
